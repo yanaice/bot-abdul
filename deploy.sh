c7229d77075c1338e835c182c86aab1fa0429081
yanaice <yana.sooksri@gmail.com>
2024-11-19 16:45:10 +0700

    add empty folder credentials

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ rm -f function.zip
 
 zip -r function.zip . -x "*.git*" "*.sh" "*.md" "node_modules/*"
 
-# gcloud functions deploy abdul-bot \
-# --gen2 \
-# --runtime=nodejs20 \
-# --region=asia-southeast1 \
-# --source=function.zip \
-# --entry-point=lineWebhook \
-# --trigger-http \
-# --allow-unauthenticated
+gcloud functions deploy abdul-bot \
+--gen2 \
+--runtime=nodejs20 \
+--region=asia-southeast1 \
+--source=function.zip \
+--entry-point=lineWebhook \
+--trigger-http \
+--allow-unauthenticated
